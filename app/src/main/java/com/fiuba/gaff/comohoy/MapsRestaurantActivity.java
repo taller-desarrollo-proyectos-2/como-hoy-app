261c36957a95f6f792cb8a744159bf2f9d3c9ed3
fernandonitz <nitzfernando@gmail.com>
2018-04-15 22:15:39 -0300

    Location Commerce with latitud and longitud or direction.

--- app/src/main/java/com/fiuba/gaff/comohoy/MapsRestaurantActivity.java
+++ app/src/main/java/com/fiuba/gaff/comohoy/MapsRestaurantActivity.java
@@ -1,7 +1,16 @@
 package com.fiuba.gaff.comohoy;
 
+import android.Manifest;
 import android.app.Dialog;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -14,6 +23,9 @@ import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MapsRestaurantActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -28,13 +40,12 @@ public class MapsRestaurantActivity extends AppCompatActivity implements OnMapRe
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
 
         GoogleApiAvailability googleAPI = GoogleApiAvailability.getInstance();
-        int resultCode = googleAPI.isGooglePlayServicesAvailable (this);
-        if (resultCode == ConnectionResult.SUCCESS){
+        int resultCode = googleAPI.isGooglePlayServicesAvailable(this);
+        if (resultCode == ConnectionResult.SUCCESS) {
             SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                     .findFragmentById(R.id.map);
             mapFragment.getMapAsync(this);
-        }
-        else{
+        } else {
             Dialog dialog = googleAPI.getErrorDialog(this, resultCode, PLAY_SERVICES_RESOLUTION_REQUEST);
             dialog.show();
         }
@@ -49,6 +60,31 @@ public class MapsRestaurantActivity extends AppCompatActivity implements OnMapRe
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
+
+    public LatLng getLocationFromAddress(String strAddress) throws IOException {
+
+        Geocoder coder = new Geocoder(this);
+        List<Address> address;
+        LatLng p1 = null;
+        try {
+            address = coder.getFromLocationName(strAddress, 5);
+            if (address == null) {
+                return null;
+            }
+            Address location=address.get(0);
+            location.getLatitude();
+            location.getLongitude();
+            p1 = new LatLng((double) (location.getLatitude() * 1E6),
+                    (double) (location.getLongitude() * 1E6));
+        }
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return p1;
+    }
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -56,10 +92,12 @@ public class MapsRestaurantActivity extends AppCompatActivity implements OnMapRe
         UiSettings uiSettings = mMap.getUiSettings();
         uiSettings.setZoomControlsEnabled(true);
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Mi item_comercio0").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+    // Add a marker in Sydney and move the camera
+
+        LatLng location = new LatLng(-34.617290, -58.367846); //PACEO COLON 850
+
+        mMap.addMarker(new MarkerOptions().position(location).title("Mi item_comercio0").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
         float zoomLevel = 16;
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,zoomLevel));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location,zoomLevel));
     }
 }
