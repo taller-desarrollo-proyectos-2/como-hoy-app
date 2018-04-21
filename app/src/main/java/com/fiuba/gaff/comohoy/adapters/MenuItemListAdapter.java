7ec49032e59e537fbbf64af18e9e8b955045f87d
Gonzalo Guzzardi <gonzaloguzzardi@gmail.com>
2018-04-21 17:31:20 -0300

    [#20] fixed plated view displaying texts in a small area, and added celiac icon to view.

--- app/src/main/java/com/fiuba/gaff/comohoy/adapters/MenuItemListAdapter.java
+++ app/src/main/java/com/fiuba/gaff/comohoy/adapters/MenuItemListAdapter.java
@@ -50,7 +50,7 @@ public class MenuItemListAdapter extends RecyclerView.Adapter<MenuItemListAdapte
 
     @Override
     public MenuItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.plato, parent, false);
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.plate_item, parent, false);
         MenuItemViewHolder menuItemViewHolder = new MenuItemViewHolder(v);
         return menuItemViewHolder;
     }
