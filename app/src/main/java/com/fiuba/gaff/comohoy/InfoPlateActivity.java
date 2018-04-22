4a2d52c7adb844be35244395f9652f4141b657cc
fernandonitz <nitzfernando@gmail.com>
2018-04-22 13:26:18 -0300

    [#20] Adding Information Plate Form

--- app/src/main/java/com/fiuba/gaff/comohoy/InfoPlateActivity.java
+++ app/src/main/java/com/fiuba/gaff/comohoy/InfoPlateActivity.java
@@ -1,12 +1,21 @@
 package com.fiuba.gaff.comohoy;
 
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -27,6 +36,37 @@ public class InfoPlateActivity extends AppCompatActivity {
         if(bundle.getString("Descripcion")!= null) { descriPlato.setText(bundle.getString("Descripcion")); }
         if(bundle.getString("Precio")!= null) { precioPlato.setText(bundle.getString("Precio")); }
 
+        Button agregarPlatoPedido = (Button) findViewById(R.id.agregar_plato_pedido);
+        agregarPlatoPedido.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        ImageButton desplegarOpcionesExtra = (ImageButton) findViewById(R.id.desplegar_opciones);
+        desplegarOpcionesExtra.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final Dialog dialog = new Dialog(InfoPlateActivity.this);
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                dialog.setContentView(R.layout.opciones_extra_plato);
+                List<String> stringList=new ArrayList<>();  // here is list
+                for(int i=0;i<5;i++) {
+                    stringList.add("RadioButton " + (i + 1));
+                }
+                RadioGroup rg = (RadioGroup) dialog.findViewById(R.id.id_opciones_extra_plato);
+                for(int i=0;i<stringList.size();i++){
+                    RadioButton rb=new RadioButton(InfoPlateActivity.this); // dynamically creating RadioButton and adding to RadioGroup.
+                    rb.setText(stringList.get(i));
+                    rg.addView(rb);
+                }
+                dialog.show();
+            }
+        });
+
     }
+
 
 }
