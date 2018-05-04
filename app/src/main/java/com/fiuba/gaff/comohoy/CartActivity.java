f266b175a9dd4aeaffd17a6c3b6a1db18367a8c9
Gonzalo Guzzardi <gonzaloguzzardi@gmail.com>
2018-05-04 18:35:56 -0300

    [#22] layouts y setup inicial para crear vista del carrito

---
+++ app/src/main/java/com/fiuba/gaff/comohoy/CartActivity.java
@@ -0,0 +1,14 @@
+package com.fiuba.gaff.comohoy;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_see_cart);
+    }
+
+}
